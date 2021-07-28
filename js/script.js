fc62ebf3e6ff7c67a723b124adf5f76bfef53cbd
D7RSVN <dreamsoul.seven@gmail.com>
2021-07-28 10:35:41 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+console.clear();
+
+const app = (() => {
+  let body;
+  let menu;
+  let menuItems;
+
+  const init = () => {
+    body = document.querySelector('body');
+    menu = document.querySelector('.menu-icon');
+    menuItems = document.querySelectorAll('.nav__list-item');
+
+    applyListeners();
+  };
+
+  const applyListeners = () => {
+    menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+};
+
+  const toggleClass = (element, stringClass) => {
+    if (element.classList.contains(stringClass)){
+    element.classList.remove(stringClass);
+    setTimeout(function () {
+      element.classList.add('hidden');
+    }, 1000);
+    }else{
+
+    // element.classList.add(stringClass);
+    element.classList.remove('hidden');
+    setTimeout(function () {
+      element.classList.add(stringClass);
+    }, 20);
+  }
+  };
+
+  init();
+})();
+
+// $(document).ready(function(){       
+//   var scroll_start = 0;
+//   var startchange = $('#profile');
+//   var offset = startchange.offset();
+//   if (startchange.length){
+//       $(document).scroll(function() { 
+//           scroll_start = $(this).scrollTop();
+//           if(scroll_start > offset.top) {
+//               $("nav").css('background-color', 'rgb(0,0,0, 0.5)');
+//           } else {
+//               $('nav').css('background-color', 'transparent');
+//           }
+//       });
+//   }
+// });
